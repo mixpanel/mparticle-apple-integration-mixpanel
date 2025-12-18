e52b20cc3d28a6cee2cda5d5022d34aed047bf39
Jared McFarland <jared@mixpanel.com>
2025-12-17 16:53:52 -0800

    chore: remove files that should be gitignored
    Remove docs/, mparticle-kit-context/, ARCHITECTURE.md, and
    PROJECT_REQUIREMENTS.md from tracking as they are in .gitignore

--- mparticle-kit-context/mixpanel-sdks/mixpanel-js/examples/typescript/src/index.ts
+++
@@ -1,6 +0,0 @@
-import './core';
-import './module';
-import './module-with-async-recorder';
-import './flags';
-
-console.log('Mixpanel TypeScript examples completed');
--- mparticle-kit-context/mixpanel-sdks/mixpanel-js/tests/vendor/core-js-polyfills.src.js
+++
@@ -1,2 +0,0 @@
-// Needed to make recent versions of Sinon work on IE10.
-require('core-js');
